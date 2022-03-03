b724ed6e0de88e23a00898555d7ce5ad29decfab
Racksoup <rack.connor1@gmail.com>
2022-03-03 15:46:50 -0500

    Created route - Update admin password

--- routes/api/users.js
+++ routes/api/users.js
@@ -9,10 +9,59 @@ const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 // @route   Post api/users
-// @desc    Make Admin
+// @desc    Make Admin. Activate to create new Admin
 // @access  Public
-router.post('/', [
-  check('username', 'Username is required').not().isEmpty(),
+// router.post('/', [
+//   check('username', 'Username is required').not().isEmpty(),
+//   check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 }),
+//   async (req, res) => {
+//     const errors = validationResult(req);
+//     if (!errors.isEmpty()) {
+//       return res.status(400).json({ errors: errors.array() });
+//     }
+
+//     const { username, password } = req.body;
+//     try {
+//       let user = await User.findOne({ username });
+
+//       if (user) {
+//         return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+//       }
+
+//       user = new User({
+//         username,
+//         password,
+//         master,
+//       });
+
+//       // Encrypt password
+//       const salt = await bcrypt.genSalt(10);
+
+//       user.password = await bcrypt.hash(password, salt);
+
+//       await user.save();
+
+//       const payload = {
+//         user: {
+//           id: user.id,
+//         },
+//       };
+
+//       jwt.sign(payload, config.get('jwtSecret'), { expiresIn: '1h' }, (err, token) => {
+//         if (err) throw err;
+//         res.json({ token });
+//       });
+//     } catch (err) {
+//       console.error(err.message);
+//       res.status(500).send('Server error');
+//     }
+//   },
+// ]);
+
+// @route   PUT api/users
+// @desc    Change Admin Password
+// @access  Private
+router.put('/', auth, [
   check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 }),
   async (req, res) => {
     const errors = validationResult(req);
@@ -20,24 +69,14 @@ router.post('/', [
       return res.status(400).json({ errors: errors.array() });
     }
 
-    const { username, password, master } = req.body;
+    const { password } = req.body;
+    const postItem = {};
+    // Encrypt password
+    const salt = await bcrypt.genSalt(10);
+
+    postItem.password = await bcrypt.hash(password, salt);
     try {
-      let user = await User.findOne({ username });
-
-      if (user) {
-        return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
-      }
-
-      user = new User({
-        username,
-        password,
-        master,
-      });
-
-      // Encrypt password
-      const salt = await bcrypt.genSalt(10);
-
-      user.password = await bcrypt.hash(password, salt);
+      let user = await User.findOneAndUpdate({ username: 'Admin' }, postItem);
 
       await user.save();
 
@@ -53,7 +92,7 @@ router.post('/', [
       });
     } catch (err) {
       console.error(err.message);
-      res.status(500).send('Server error');
+      res.status(500).send('Server Error');
     }
   },
 ]);
